d4d7cdbb0ccef737c06fd5e41190ed43bdc231a2
Allen Porter <allen@thebends.org>
2022-12-27 15:55:50 -0800

    Add initial import of hostdb

---
+++ hostdb/allocate_hostname.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# Tool for allocating a hostname from a list of words and set of existing
+# hosts.  The tool prints out a new hostname that should be added to the
+# database.
+
+import argparse
+
+from . import hostdb
+
+# Define command line arguments
+parser = argparse.ArgumentParser(description="Allocate a hostname")
+parser.add_argument(
+    "--num", type=int, default=5, help="Number of hostnames to allocate"
+)
+
+HOSTDB_DIRS = ["hosts/prod", "hosts/dev"]
+
+
+def main():
+    args = parser.parse_args()
+    dbs = [hostdb.HostDb(path) for path in HOSTDB_DIRS]
+    new_hosts = hostdb.allocate_hostnames(dbs, args.num)
+    for host in new_hosts:
+        print(host)
+
+
+if __name__ == "__main__":
+    main()
